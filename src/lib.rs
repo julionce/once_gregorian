de4a7066835e59d837a2ec38b5fcdee89281fdec
julionce <julianbortega@gmail.com>
2023-09-23 15:47:06 +0200

    Add Date, Year, Month, DayOfMonth and DayOfYear types

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,34 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+pub type Year = u16;
+pub type Month = u8;
+pub type DayOfMonth = u8;
+pub type DayOfYear = u16;
+
+#[derive(Debug, Clone, Copy)]
+pub struct Date {
+    year: Year,
+    month: Month,
+    day: DayOfMonth,
+}
+
+impl Date {
+    pub fn from_year_month_day(year: Year, month: Month, day: DayOfMonth) -> Self {
+        Self { year, month, day }
+    }
+
+    pub fn year(&self) -> Year {
+        self.year
+    }
+
+    pub fn month(&self) -> Month {
+        self.month
+    }
+
+    pub fn day_of_month(&self) -> DayOfMonth {
+        self.day
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
 }
